2c8039f3e604dd69b76764a2259d6987a68d0e8e
baishuigansijun <1069236724@qq.com>
2025-04-18 18:35:31 +0800

    init

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,26 +1,38 @@
-import { defineConfig } from 'wxt';
+import {defineConfig} from 'wxt';
 import react from '@vitejs/plugin-react';
 
 console.log('📦 wxt.config.ts loaded')
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  // modules: ['@wxt-dev/module-react'],
-  srcDir: 'src',
-  vite:()=>(
-      {
-        plugins: [
-          // 用 Babel 版的 React 插件，它会自动读取根目录的 babel.config.cjs
-          react({
-              babel: {
-                  // Use babel.config.cjs files
-                  configFile: true,
-              }
-          })
-        ],
-        resolve: {
-          alias: { '@': '/src' }
+    // modules: ['@wxt-dev/module-react'],
+    srcDir: 'src',
+    vite: () => (
+        {
+            plugins: [
+                react({
+                    babel: {
+                        // Use babel.config.cjs files
+                        configFile: true,
+                    }
+                })
+            ],
+            resolve: {
+                alias: {'@': '/src'}
+            }
         }
-      }
-  )
+    ),
+    manifest: {
+        permissions: [
+            'storage',
+            'tabs',
+            'scripting',
+            "activeTab",
+            "alarms"
+        ],
+        host_permissions: [
+            // '<all_urls>',
+            'https://api.example.com/*'
+        ],
+    }
 });
